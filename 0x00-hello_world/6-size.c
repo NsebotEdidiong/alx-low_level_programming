54ea914731244014575ee5380bea05ed054658e6
NsebotEdidiong <officialnew008@gmail.com>
2023-06-24 09:13:11 -0700

    size script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - to print characters
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("size of char: %lu", sizeof(char));
+	printf("size of an int: %lu", sizeof(int));
+	printf("size of a long int: %lu", sizeof(long int));
+	printf("size of a long long int: %lu", sizeof(long) - ~~~long int);
+	printf("size of a float: %lu", sizeof(float));
+	return (0);
+}
